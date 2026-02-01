49b8712cb9b95737db3053624f88062453c98b30
Raghavendar-S <raghav.vasavi@gmail.com>
2026-02-01 20:34:13 +0530

    Refactor Layout component to use className for main content and enhance Navbar styling with z-index and shadow

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
-      <main style={{ minHeight: "100vh" }}>{children}</main>
+      <main className="content">{children}</main>
       <Footer />
     </div>
   );
